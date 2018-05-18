02d76c25e92d4af0a9321dbc8e0ad151f26f2676
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-18 09:41:44 +0200

    bottom nav stup

--- app/src/main/java/cz/levinzonr/archat/MainActivity.kt
+++ app/src/main/java/cz/levinzonr/archat/MainActivity.kt
@@ -2,13 +2,17 @@ package cz.levinzonr.archat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
 import cz.levinzonr.archat.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
@@ -16,4 +20,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            else -> false
+        }
+    }
 }

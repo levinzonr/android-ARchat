14ce26dfa44ad4a4ff8b3c2ac9753bc486d63bd6
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-18 18:54:24 +0200

    new navigation stuff set-up

--- app/src/main/java/cz/levinzonr/archat/MainActivity.kt
+++ app/src/main/java/cz/levinzonr/archat/MainActivity.kt
@@ -5,8 +5,9 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.view.MenuItem
 import cz.levinzonr.archat.ui.main.MainFragment
+import cz.levinzonr.archat.ui.preferences.PreferencesFragment
 import kotlinx.android.synthetic.main.main_activity.*
-
+import cz.levinzonr.archat.R
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +23,16 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager?.beginTransaction()
+                        ?.replace(R.id.container, MainFragment.newInstance())?.commit()
+                true
+            }
+            R.id.navigation_settings -> {
+                supportFragmentManager?.beginTransaction()
+                        ?.replace(R.id.container, PreferencesFragment())?.commit()
+                true
+            }
             else -> false
         }
     }
